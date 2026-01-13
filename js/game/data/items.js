7e3fe1facf4677fa8520b879319668a44dad08f1
Maximilian Horn <46897140+MaximilianHxrn@users.noreply.github.com>
2026-01-13 10:47:40 -0800

    Add items data file to js/game/data directory

---
+++ js/game/data/items.js
@@ -0,0 +1,7 @@
+export const ITEMS = {
+  rawMeat: { id: 'rawMeat', name: 'Raw Meat', color: '#cc4444', value: 1 },
+  wood: { id: 'wood', name: 'Wood', color: '#995522', value: 0.5 },
+  processedMeat: { id: 'processedMeat', name: 'Processed Meat', color: '#aa2233', value: 3 },
+  crudeOil: { id: 'crudeOil', name: 'Crude oil', color: '#333333', value: 1 },
+  diesel: { id: 'diesel', name: 'Diesel', color: '#333399', value: 2 },
+};
